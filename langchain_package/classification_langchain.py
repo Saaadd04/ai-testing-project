909bf87532debd90beb30f3599949b9fdb826d27
Saaadd04 <saad.infomover@gmail.com>
2025-04-15 10:14:44 +0530

    task done

---
+++ langchain_package/classification_langchain.py
@@ -0,0 +1,57 @@
+
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_mistralai import ChatMistralAI
+# from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+import os
+
+load_dotenv()
+
+def load_api_key():
+    key = os.getenv("MISTRAL_API_KEY")
+    if key:
+        print("API key loaded successfully.")
+    else:
+        raise ValueError("MISTRAL_AI API key not found in .env file.")
+
+# === Prompt Template (Common for both cases) ===
+def get_prompt():
+    return ChatPromptTemplate.from_template(
+        """
+        Extract the desired information from the following passage.
+        Only extract the properties mentioned in the 'Classification' function.
+
+        Passage:
+        {input}
+        """
+    )
+
+
+class Simple_Classification(BaseModel):
+    sentiment: str = Field(description="The sentiment of the text")
+    aggressiveness: int = Field(description="Aggression level from 1 to 10")
+    language: str = Field(description="Language of the text")
+
+
+
+
+
+def run(input_text: str, mode: str = "finer"):
+
+    model_schema = Simple_Classification
+
+    llm = ChatMistralAI(temperature=0, model="mistral-large-latest").with_structured_output(model_schema)
+    prompt = get_prompt().invoke({"input": input_text})
+    response = llm.invoke(prompt)
+
+    print(f"\n Classification Mode: {mode.upper()}")
+    print("Input:", input_text)
+    print("Output:", response.model_dump())
+
+if __name__ == "__main__":
+    load_api_key()
+
+    # Change text and mode here to switch between classification types
+    input_text = input("Enter a text to test: ")
+    run(input_text, mode="simple")
